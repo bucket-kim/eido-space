c143a17173821639023d0f2ebb194d2c1157cb98
Kyoung Hoon Kim <bucketkim@kyoungucbookpro.myfiosgateway.com>
2023-04-24 15:16:38 -0400

    start to node

--- backend/errorController.js
+++ backend/errorController.js
@@ -22,7 +22,7 @@ module.exports = (err, req, res, next) => {
   try {
     if (err.name === "ValidationError")
       return (err = handleValidationError(err, res));
-    if (err.code && err.code == 11000)
+    if (err.code && err.code === 11000)
       return (err = handleDuplicateKeyError(err, res));
   } catch (err) {
     res.status(500).send("An unknown error occurred.");
